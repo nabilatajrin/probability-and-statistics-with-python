3bc5c92ca3d42bbfa804878fc8fdc93ea3b90acb
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-05-20 12:48:47 +0600

    Create 02.probability-as-relative-frequency.py
    first commit

---
+++ probability-in-python/02.probability-as-relative-frequency.py
@@ -0,0 +1,10 @@
+#The empirical probability of an event is nothing but the relative frequency (proportion or percentage) 
+#of that event with respect to the total number of times the experiment was performed.
+
+
+
+p_heads_1=(300-162)/300
+percentage_1=p_heads_1*100
+
+p_heads_2=(5000-2450)/5000
+percentage_2=p_heads_2*100
