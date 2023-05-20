53ec9d6e9b48483ec9a27b5022b5fc68677986e4
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-05-20 10:23:32 +0600

    Create 1.the-empirical-probability.py

---
+++ probability-in-python/1.the-empirical-probability.py
@@ -0,0 +1,18 @@
+#Whenever we can't predict outcomes with certainty, we're dealing with a random experiment.
+#Although we can't predict the outcome of a random experiment, we can at least estimate the probability (the chances) associated with its outcomes.
+#A coin toss has two possible outcomes, and we can estimate the probability associated with the coin landing on heads or tails.
+#In probability notation, we could write this as: Probability(coin lands heads up) = H/Total number of tosses = 0.56
+#For simplicity, the notation is often compressed, and "Probability" becomes "P", while "coin lands heads up" becomes simply "heads" or H: P(H) = 0.56
+#In other resources, we may find a slightly different notation, like p(H) or Pr(H), but P(H) is generally preferred.
+#Generally, for any event E (like a coin landing heads up), we can find its probability by using the following formula: 
+#P (E) = number of times event E happened / number of times we repeated the experiment.
+#experimental — or empirical probability.
+
+#Print p_tail, p_six, and p_odd to examine the probabilities:
+p_tail=44/100.00
+p_six=28.00/200.00
+p_odd=102.00/200.00
+
+print(p_tail)
+print(p_six)
+print(p_odd)
