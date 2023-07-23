a7824eedc8880a51e02ea15c1f83973610bd77d6
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-07-23 22:16:50 +0600

    Update 01-flipping-coins.py

--- probability-in-python/01-flipping-coin/01-flipping-coins.py
+++ probability-in-python/01-flipping-coin/01-flipping-coins.py
@@ -7,3 +7,8 @@ np.random.seed(42)
 # Simulate one coin flip with 35% chance of getting heads
 coin_flip = bernoulli.rvs(p=0.35, size=1)
 print(coin_flip)
+
+# Simulate ten coin flips and get the number of heads
+ten_coin_flips = bernoulli.rvs(p=0.35, size=10)
+coin_flips_sum = sum(ten_coin_flips)
+print(coin_flips_sum)
