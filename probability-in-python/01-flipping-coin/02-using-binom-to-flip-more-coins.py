63317a228a2cd5cb1cfc5cce02e6de0c3fa73fc1
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-09-19 20:21:10 +0600

    Update 02-using-binom-to-flip-more-coins.py

--- probability-in-python/01-flipping-coin/02-using-binom-to-flip-more-coins.py
+++ probability-in-python/01-flipping-coin/02-using-binom-to-flip-more-coins.py
@@ -1,5 +1,5 @@
-# Import the bernoulli object from scipy.stats
-from scipy.stats import bernoulli
+# Set the random seed to reproduce the results
+np.random.seed(42)
 
 # Simulate 20 trials of 10 coin flips 
 draws = binom.rvs(p=0.35, size=20, n=10)
